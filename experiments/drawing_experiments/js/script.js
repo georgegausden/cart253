6be6aeef2ba7410d14b62d413978d57e36e3a948
georgegausden <george.philip.gausden@gmail.com>
2021-09-08 17:56:05 -0400

    added colour information tutorial

--- experiments/drawing_experiments/js/script.js
+++ experiments/drawing_experiments/js/script.js
@@ -14,12 +14,18 @@ function setup() {
 
   createCanvas(500,500);
 
-  background(100,200,200);
+  background(255,127,0);
+
+  //fill applies to all the shapes after it
+  stroke(0,255,0);
+  fill(127,0,200, 50);
 
   //rectmode(center) makes us draw x and y in center instead of corner
   //this works for other shapes too ellipseMode(CENTER)...
   rectMode(CENTER);
   rect(250,250,100,100);
+
+  fill(128,10,10,100)
   rect(250,250,50,50);
 
   ellipse(250,250,100,100);
