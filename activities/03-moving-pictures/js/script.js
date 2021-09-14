ef3a9381e2d57eef3d4249d657d732770cc78d14
georgegausden <89808071+georgegausden@users.noreply.github.com>
2021-09-14 19:46:54 -0400

    did the tutorial for the moving circles

--- activities/03-moving-pictures/js/script.js
+++ activities/03-moving-pictures/js/script.js
@@ -7,10 +7,37 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
+let bg = {
+  r:0,
+  g:0,
+  b:0
+};
+
+let circle1={
+  x:0,
+  y:250,
+  size:100,
+  fill:255,
+  speed:1,
+  growthRate:1,
+  alpha:200
+};
+
+let circle2={
+  x:500,
+  y:250,
+  size:75,
+  fill:255,
+  speed:-1,
+  sizeRatio:0.75,
+  alpha:200
+};
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500,500);
+  noStroke();
 
 }
 
@@ -18,5 +45,21 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  //background
+  background(bg.r,bg.g,bg.b);
+  bg.r= map(circle1.size,100,width,0,255);
+  //left circle
+  circle1.x+=circle1.speed;
+  circle1.x = constrain(circle1.x,0,width/2);
+  circle1.size +=circle1.growthRate;
+  circle1.size = constrain(circle1.size,0,width);
+  fill(circle1.fill,circle1.alpha);
+  ellipse(circle1.x,circle1.y,circle1.size);
 
+  //right circle
+  circle2.x+=circle2.speed;
+  circle2.x=constrain(circle2.x,width/2,width)
+  circle2.size = circle1.size*circle2.sizeRatio;
+  fill(circle2.fill,circle2.alpha);
+  ellipse(circle2.x,circle2.y,circle2.size);
 }
