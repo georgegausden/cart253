838a40bca48ddc781726af99dd41dff1e65a51e6
georgegausden <george.philip.gausden@gmail.com>
2021-09-15 09:25:28 -0400

    E1: created my three shapes for the program, set up canvas

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -17,10 +17,45 @@ Use map() and constrain()
 Respond to the mouse position using mouseX and mouseY
 **************************************************/
 
+//create JavaScript Objects for the different parameters we're going to change in draw()
+let backgroundColor:{
+  r:0,
+  g:0,
+  b:0
+};
+
+let circle1:{
+  x:0,
+  y:250,
+  size:100,
+  speed:1,
+  fill:(200,0,0)
+};
+
+let circle2:{
+  x:500,
+  y:250,
+  size:50,
+  speed:0.5,
+  fill:(0,200,0)
+};
+
+let triangle1:{
+  x1:100,
+  y1:250,
+  x2:200,
+  y2:300,
+  x3:400,
+  y3:400,
+  fill:(120,20,30)
+};
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500,500);
+  background(0)
 
 }
 
@@ -28,5 +63,9 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  noStroke();
+
+
+
 
 }
