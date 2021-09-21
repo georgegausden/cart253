57a8a9f4becfaa845636a9bc18421581d643a0e0
georgegausden <george.philip.gausden@gmail.com>
2021-09-21 16:09:50 -0400

    followed the tutorial

--- experiments/conditionals - experiments/js/script.js
+++ experiments/conditionals - experiments/js/script.js
@@ -1,22 +1,41 @@
-"use strict";
+let backgroundShade = 0;
+let circle = {
+  x:0,
+  y:250,
+  size:100,
+  speed:5
+}
 
-/**************************************************
-Template p5 project
-Pippin Barr
 
-Here is a description of this template p5 project.
-**************************************************/
 
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
-  background(255,0,0);
+  createCanvas(500,500);
+
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(backgroundShade);
 
-}e
+  circle.x = circle.x + circle.speed;
+
+  if (circle.x > width){
+    circle.speed = -circle.speed
+  }
+
+  if (circle.x < 0){
+    circle.speed = -circle.speed
+  }
+
+  ellipse(circle.x,circle.y,circle.size);
+
+  if (mouseY < height/2){
+    fill(255,0,0);
+  }
+
+  if (mouseY > height/2){
+    fill(0,0,255);
+  }
+}
