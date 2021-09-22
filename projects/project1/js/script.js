c39d6bc91febbe9528a51bc370a909aac8dc34e2
georgegausden <89808071+georgegausden@users.noreply.github.com>
2021-09-21 20:32:49 -0400

    P1 : setup and the ground

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -7,10 +7,18 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+
+  //set up the ground
+  stroke(255);
+  line(0,height/2,width,height/2);
+
 
 }
 
