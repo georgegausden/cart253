2bcdb71bf12d3574bfbeec613e5dc50cf3be1618
georgegausden <89808071+georgegausden@users.noreply.github.com>
2021-09-21 21:04:43 -0400

    P1 : created the arms and the legs and the body of the character

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -8,10 +8,12 @@ Here is a description of this template p5 project.
 **************************************************/
 //create the basic character
 let character = {
+  //create the initial position variables for the character
+  xiposition: 50,
   //create the head (circle)
   headx: undefined,
   heady: undefined,
-  headsize: 20,
+  headsize: 40,
 
   //create the body
   bodyx1: 0,
@@ -19,6 +21,15 @@ let character = {
   bodyx2: 0,
   bodyy2: 0,
   height: 100,
+
+  //create the legs
+  legheight: 20,
+  hips: 20,
+
+  //create the arms
+  neckToArms: 40,
+  armsToBody: 20,
+  armLength: 20,
 };
 
 // setup()
@@ -32,9 +43,23 @@ function setup() {
   stroke(255);
   line(0,height/2,width,height/2);
 
-  //set the position of the character
-  character.headx = 0;
+  //set the position of the character's head
+  character.headx = character.xiposition;
   character.heady = windowHeight/2 - character.height;
+  //set the position of the character's body
+
+
+  //draw the character's head
+  circle(character.headx, character.heady, character.headsize);
+  //draw the character's body
+  stroke(255);
+  line(character.headx, character.heady, character.headx, height/2-character.legheight);
+  //draw the character's legs
+  line(character.headx, height/2 - character.legheight, character.headx + character.hips, height/2 );
+  line(character.headx, height/2 - character.legheight, character.headx - character.hips, height/2 );
+  //draw the character's arms
+  line(character.headx, character.heady + character.neckToArms, character.headx + character.armsToBody, character.heady + character.armLength);
+  line(character.headx, character.heady + character.neckToArms, character.headx - character.armsToBody, character.heady + character.armLength);
 
 
 }
@@ -44,7 +69,6 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
 
-  //draw the character's head
-  
-  circle(character.headx, character.heady, character.headsize);
+  //
+
 }
