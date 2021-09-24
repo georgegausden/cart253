adbcd1dd6d4a5b077454f1678ef0b43a52ff2d1a
georgegausden <george.philip.gausden@gmail.com>
2021-09-24 17:45:22 -0400

    added new tutorials

---
+++ experiments/conditionals - experiments 4/js/script.js
@@ -0,0 +1,50 @@
+let circle = {
+  x: 250,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0,
+  acceleration:0.1,
+  maxSpeed: 5,
+};
+
+
+
+function setup() {
+  createCanvas(500,500);
+
+}
+
+
+function draw() {
+  background(0);
+
+  if (mouseX < circle.x){
+    circle.ax = -circle.acceleration;
+  }
+  else{
+    circle.ax = circle.acceleration;
+  }
+
+  if (mouseY < circle.y){
+    circle.ay = -circle.acceleration;
+  }
+  else{
+    circle.ay = circle.acceleration;
+  }
+
+  circle.vx = circle.vx + circle.ax;
+  circle.vx = constrain(circle.vx, -circle.maxSpeed, circle.maxSpeed);
+  circle.vy = circle.vy + circle.ay;
+  circle.vy = constrain(circle.vy, -circle.maxSpeed, circle.maxSpeed);
+
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
+
+  ellipse(circle.x, circle.y, circle.size);
+
+
+
+}
