5763ff34b55e85daa2723167dc8f114c8d195383
georgegausden <george.philip.gausden@gmail.com>
2021-09-25 15:10:27 -0400

    Ex: set the police cars up and their positions in setup

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -4,19 +4,49 @@
 Template p5 project
 Pippin Barr
 
-Here is a description of this template p5 project.
+A program where the user has to run away from cars
 **************************************************/
+
+let policeCar1 = {
+  x: ,
+  y: ,
+  vx: ,
+  vy: ,
+  speed: ,
+  ax: ,
+  ay: ,
+  acceleration: ,
+}
+
+let policeCar2 = {
+  x: ,
+  y: ,
+  vx: ,
+  vy: ,
+  speed: ,
+  ax: ,
+  ay: ,
+  acceleration: ,
+}
+//
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(windowWidth,windowHeight);
+  //set the policeCar1 position
+  policeCar1.x = random(0,windowWidth);
+  policaCar1.y = random(0,windowHeight);
 
+  //set the policeCar2 position
+  policeCar2.x = random(0,windowWidth);
+  policaCar2.y = random(0,windowHeight);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
+  //set the
 }
