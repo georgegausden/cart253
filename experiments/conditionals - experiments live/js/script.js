4afcee6b541bb235184b566d724ea59f7c30530e
georgegausden <george.philip.gausden@gmail.com>
2021-09-28 14:08:43 -0400

    live: face looks left and right

--- experiments/conditionals - experiments live/js/script.js
+++ experiments/conditionals - experiments live/js/script.js
@@ -1,13 +1,45 @@
+"use strict";
 
+/**
+
+*multiple images (happy face/scary face)
+*for loops (discomfort with, why are they instant?)
+*delaying code
+*pushing and popping
+
+
+*/
+
+let normalFace = undefined;
+let scaredFace = undefined;
+
+function preload() {
+  normalFace = loadImage('assets/images/clown.png');
+  scaredFace = loadImage('assets/images/scaredface.png');
+}
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(500,500);
 
 
 }
 
 
 function draw() {
+  background(0);
 
-  
+  imageMode(CENTER);
+  //looking left
+  translate(width / 2, height / 2);
+  if (mouseX < width/2){
+    scale(-1,1);
+    image(scaredFace, 0, 0, 300, 300);
+
+  }
+  else{
+    scale(1,1);
+    image(scaredFace, 0, 0, 300, 300);
+  }
+
+
 }
