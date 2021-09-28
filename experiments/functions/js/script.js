43844bfec88a5fc843a8c12f74357d701469e794
georgegausden <george.philip.gausden@gmail.com>
2021-09-28 16:04:55 -0400

    watched tutorial for functions

--- experiments/functions/js/script.js
+++ experiments/functions/js/script.js
@@ -1,53 +1,48 @@
-"use strict";
-
-/**
-
-*multiple images (happy face/scary face)
-*for loops (discomfort with, why are they instant?)
-*delaying code
-*pushing and popping
-
-
-*/
-
-let normalFace = undefined;
-let scaredFace = undefined;
-
-let face = {
-  x: 250,
-  y:250,
-  terrorThreshold: 20
-}
-
-function preload() {
-  normalFace = loadImage('assets/images/clown.png');
-  scaredFace = loadImage('assets/images/scaredface.png');
-}
+let circle = {
+  x:0,
+  y: 250,
+  size: 100,
+  vx: 1,
+  vy: 0
+};
 
 function setup() {
   createCanvas(500,500);
 
-
 }
-
 
 function draw() {
   background(0);
+  //turn all of draw into just functions
+  move();
+  wrap();
+  display();
+}
 
-  imageMode(CENTER);
-  //looking left
-  translate(width / 2, height / 2);
-  if (mouseX < width/2){
-    scale(-1,1);
-    image(scaredFace, 0, 0, 300, 300);
-
+function wrap(){
+  if (circle.x > width) {
+    reset();
   }
-  else{
-    scale(1,1);
-    image(scaredFace, 0, 0, 300, 300);
-  }
+}
 
-  let d = dist(mouseX, mouseY, face.x, face.y);
-  
+function mousePressed(){
+  reset();
+}
+
+function reset(){
+  circle.x = 0;
+  circle.vx += 2;
+  circle.vy -= 0.25;
+  circle.size +=5;
+}
+
+function move(){
+  circle.x +=circle.vx;
+  circle.y += circle.vy
+}
+
+function display(){
+  fill(255,0,0);
+  ellipse(circle.x,circle.y,circle.size);
 
 }
