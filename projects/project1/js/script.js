9c6ea7ce205d2af38c5c5ec0a53b470597e30266
georgegausden <george.philip.gausden@gmail.com>
2021-10-04 15:02:32 -0400

    P1: created the mouse pressed and simulation function

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -6,26 +6,59 @@ George Gausden
 
 Here is a description of this template p5 project.
 **************************************************/
+//create the javascript objects and the variables in the program
+
+//setup the initial state as the title
+let state = "title";
+
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
   //setup the title, simulation and end states
-  if (state = "title"){
+  if (state === "title"){
     title();
   }
-  else if (state = "simulation"){
+  else if (state === "simulation"){
     simulation();
   }
-  else if (state = "end"){
+  else if (state === "end"){
     end();
   }
+}
+
+//create the title function
+function title(){
+  push();
+  textSize(64);
+  fill(100, 100, 255);
+  textAlign(CENTER, CENTER);
+  text("Babes escaped!\nGo catch her!\nUse the arrow\nkeys to move", width / 2, height / 2);
+  pop();
+}
+
+//create a way to move from the title function to the simulation function
+function mousePressed(){
+  if (state === "title"){
+    state = "simulation";
+  }
+}
+
+//create the simulation function
+function simulation(){
+  push();
+  textSize(64);
+  fill(100, 100, 255);
+  textAlign(CENTER, CENTER);
+  text("This is a simulation", width / 2, height / 2);
+  pop();
 }
