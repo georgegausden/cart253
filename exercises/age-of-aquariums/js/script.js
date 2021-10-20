7d38e0ca2f86ce555ee15c7f9e3ef53f74166caf
georgegausden <89808071+georgegausden@users.noreply.github.com>
2021-10-20 13:18:12 -0400

    Ex3: added doctor fish functions and a new immune fish array

--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -9,12 +9,16 @@ author, and this description to match your project!
 "use strict";
 
 let school = [];
-let schoolSize = 3;
-let distanceOfInfection = 1;
+let schoolSize = 100;
+let distanceOfInfection = 0;
 
 //create the array for infected fish
 let infectedFish = [];
 let initialInfected = 1;
+
+let immuneFish = [];
+let doctorFish = undefined;
+let initialImmune = 1;
 
 
 //create a virus that's introduced into the simulation
@@ -44,16 +48,20 @@ function setup() {
     infectedFish.push(fish);
   }
 
+  let doctorFish = createFish(random(0,width), random(0,height));
+  immuneFish.push(doctorFish);
+
+
 }
 
 function createFish(x,y){
   let fish = {
     x: x,
     y: y,
-    size: 60,
+    size: 20,
     vx: 0,
     vy: 0,
-    speed: 5
+    speed: 2
   };
   return fish;
 }
@@ -64,10 +72,10 @@ Description of draw()
 function draw() {
   background(255);
 
-    for (let i = 0; i < school.length; i++){
-      moveFish(school[i]);
-      displayFish(school[i]);
-    }
+  for (let i = 0; i < school.length; i++){
+    moveFish(school[i]);
+    displayFish(school[i]);
+  }
 
 
   for (let i = 0; i < infectedFish.length; i++){
@@ -75,12 +83,20 @@ function draw() {
     displayInfectedFish(infectedFish[i]);
   }
 
+  for (let i = 1; i < immuneFish.length; i++){
+    displayImmuneFish(immuneFish[i]);
+  }
+
+  displayDoctorFish(immuneFish[0]);
+  move_doctor_fish();
+
+
   //check if the infected fish comes into contact with a normal fish
   //checkTouch();
 
 
 
-  for (let i = 0; i < school.length; i++){
+  for (let i = 0; i < school.length -1; i++){
     for (let j = 0; j < infectedFish.length; j++){
       if (school.length > 0){
         if (checkTouch(infectedFish[j], school[i])){
@@ -109,6 +125,22 @@ function displayInfectedFish(fish){
   pop();
 }
 
+function displayImmuneFish(fish){
+  push();
+  fill(0,255,0);
+  noStroke();
+  ellipse(fish.x,fish.y,fish.size);
+  pop();
+}
+
+function displayDoctorFish(fish){
+  push();
+  fill(0,255,255);
+  noStroke();
+  ellipse(fish.x,fish.y,fish.size);
+  pop();
+}
+
 function moveFish(fish){
   let change = random(0,1);
   if (change < 0.05){
@@ -126,7 +158,7 @@ function moveFish(fish){
 
 function displayFish(fish){
   push();
-  fill(0,255,0);
+  fill(0,0,255);
   noStroke();
   ellipse(fish.x,fish.y,fish.size);
   pop();
@@ -139,5 +171,19 @@ function checkTouch(fish1, fish2){
   if (d <= (fish1.size/2 + fish2.size/2 + distanceOfInfection)){
     return true;
   }
+
+}
+
+function move_immune_fish(){
+
+}
+
+function move_doctor_fish(){
+  immuneFish[0].x = mouseX;
+  immuneFish[0].y = mouseY;
+}
+
+//give immunity to the fish
+function give_immunity(healthyArray){
 
 }
