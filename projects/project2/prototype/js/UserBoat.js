6647151619f9b501ff74e2eb16818b033dab9b55
georgegausden <george.philip.gausden@gmail.com>
2021-11-01 15:14:04 -0400

    P2: added states to the program

--- projects/project2/prototype/js/UserBoat.js
+++ projects/project2/prototype/js/UserBoat.js
@@ -2,6 +2,7 @@ class UserBoat extends Boat{
 
   constructor(x,y){
     super(x,y);
+    //set the number of lives available to the user
     lives = 5;
   }
 
--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -9,6 +9,9 @@ In this prototype I want to create the skeleton of the moving character and the 
 //create the character class
 let user;
 let enemy;
+//set the intial state of the game
+let state = 'title';
+
 // setup()
 //
 // Description of setup() goes here.
@@ -27,12 +30,42 @@ function setup() {
 function draw() {
   background(0);
 
+  //the three possible states of the game are title(), simulation() and end()
+  if (state === 'title'){
+    title();
+  }
+  else if (state === 'simulation'){
+    simulation();
+  }
+  else if (state === 'end'){
+    end();
+  }
+}
+
+//create the states for the game
+function title(){
+  background(255);
+
+  push();
+  textSize(50);
+  text("Title Page",width/2,height/2);
+  pop();
+
+  if (keyIsDown(13)){
+    state = 'simulation'
+  };
+}
+
+function simulation(){
   //make the user display
   user.display();
   user.move();
   enemy.display();
   enemy.move();
+}
 
-
-
+function end(){
+  push();
+  text("The End",width/2,height/2);
+  pop();
 }
