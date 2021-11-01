173094e578a00d0d6c23cfa2cc99ae17ef5a58b4
georgegausden <george.philip.gausden@gmail.com>
2021-11-01 11:54:38 -0400

    P2: created the character class and added it to the main script

--- projects/project2/prototype/js/Character.js
+++ projects/project2/prototype/js/Character.js
@@ -1,5 +1,35 @@
 class Character {
-  constructor{
-    x:
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.vx = 2;
+    this.vy = 2;
+    this.size = 10;
+    this.fill = 255;
   }
+
+  move(){
+    //make the user move depending on what key they press
+    if (keyIsDown(68)){
+      this.x += this.vx
+    }
+    else if (keyIsDown(65)){
+      this.x -= this.vx
+    }
+    else if (keyIsDown(87)){
+      this.y -= this.vy
+    }
+    else if (keyIsDown(83)){
+      this.y += this.vy
+    };
+  }
+
+  display(){
+    //display the user on the screen as a circle
+    push();
+    fill(this.fill);
+    circle(this.x,this.y,this.size);
+    pop();
+  }
+  
 }
--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -7,17 +7,26 @@ Pippin Barr
 In this prototype I want to create the skeleton of the moving character and the user being able to control it
 **************************************************/
 //create the character class
-
+let user;
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(600,600);
 
+  //create the character
+  user = new Character(width/2,height/2);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
+
+  //make the user display
+  user.display();
+  user.move();
+
 
 }
