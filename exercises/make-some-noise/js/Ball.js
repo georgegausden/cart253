c356e88e4e98ed1177a882541d800af45f9861f8
georgegausden <george.philip.gausden@gmail.com>
2021-11-10 08:59:18 -0500

    Ex: added a ghost image and a check touch function

--- exercises/make-some-noise/js/Ball.js
+++ exercises/make-some-noise/js/Ball.js
@@ -29,17 +29,13 @@ class Ball {
     this.x += this.vx;
     this.y += this.vy;
 
-    // Update frequency
-    let d = dist(this.x,this.y,width/2,height/2);
-    let maxDist = dist(0,0,width/2,height/2);
-    let newFreq = map(d,0,maxDist,this.nearFreq,this.farFreq);
-    this.oscillator.freq(newFreq);
   }
 
   bounce() {
     if (this.x - this.size/2 < 0 || this.x + this.size/2 > width) {
       this.vx = -this.vx;
       this.playNote();
+      //touch the side of the screen, change the boolean value to true
       if (touchSideBoolean){
         touchSideBoolean = false;
       }
@@ -52,6 +48,7 @@ class Ball {
     if (this.y - this.size/2 < 0 || this.y + this.size/2 > height) {
       this.vy = -this.vy;
       this.playNote();
+      //touch the side of the screen, change the boolean value to true
       if (touchSideBoolean){
         touchSideBoolean = false;
       }
@@ -66,11 +63,20 @@ class Ball {
   }
 
   display() {
-    push();
-    noStroke();
-    fill(this.fill.r,this.fill.g,this.fill.b);
-    ellipse(this.x,this.y,this.size);
-    pop();
+
+    if (touchSideBoolean){
+      //display the ghost image
+      imageMode(CENTER);
+      image(ghostImage,this.x,this.y,this.size,this.size);
+    }
+    else{
+      push();
+      noStroke();
+      fill(this.fill.r,this.fill.g,this.fill.b);
+      ellipse(this.x,this.y,this.size);
+      pop();
+    }
+
   }
 
 }
--- exercises/make-some-noise/js/script.js
+++ exercises/make-some-noise/js/script.js
@@ -13,7 +13,16 @@ let balls = [];
 // F-minor
 let notes = [`F3`,`G3`,`Ab4`,`Bb4`,`C4`,`Db4`,`Eb4`,`F4`];
 
+
+//keep track if one of the balls touches the side of the screen
 let touchSideBoolean = false;
+
+//get a ghost image
+let ghostImage = undefined;
+
+function preload(){
+  ghostImage = loadImage('assets/images/ghost.png');
+}
 
 // setup()
 //
@@ -29,10 +38,10 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   if (touchSideBoolean){
-    background(255);
+    background(0);
   }
   else{
-    background(0);
+    background(255);
   }
 
   for (let i = 0; i < balls.length; i++) {
@@ -41,14 +50,48 @@ function draw() {
     ball.bounce();
     ball.display();
   }
+
+  //check if the balls touch. If they do, change scale
+  for (let i = 0; i<balls.length; i++){
+    for (let j = 0; j<balls.length; j++){
+      if (checkTouch(balls[i],balls[j],balls)){
+        console.log('touch');
+      }
+    }
+  }
 }
 
 function mousePressed() {
   createBall(mouseX,mouseY);
 }
 
+function keyPressed(){
+  if (keyIsDown(68)){
+    createBall(mouseX,mouseY);
+  }
+}
+
 function createBall(x,y) {
   let note = random(notes);
   let ball = new Ball(x,y,note);
   balls.push(ball);
+}
+
+//function to check if two objects touch
+function checkTouch(object1,object2,array){
+  if (array.length<2){
+    return false
+  }
+  else{
+
+    let d = dist(object1.x,object1.y,object2.x,object2.y);
+
+    if (d<=(object1.size/2+object2.size/2)){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+
 }
